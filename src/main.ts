4226121f719b3835d3ef55a951808e84d267afaa
mounikaruppa <mounikaruppa1981@gmail.com>
2025-08-25 18:42:46 +0530

    Delete src directory

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { MovieListComponent } from './app/movie-list/movie-list';
-
-bootstrapApplication(MovieListComponent, {
-  providers: [provideAnimations()]
-});
